3437d22e6dbebcf701185526647c40af1adbfd81
Pinusa Trakoolpaiboolpol <pinusa.tk@gmail.com>
2022-07-03 08:03:16 +0700

    update

--- Case 2 - Pizza Runner/Part C. Ingredient Optimization.sql
+++ Case 2 - Pizza Runner/Part C. Ingredient Optimization.sql
@@ -1,31 +1,31 @@
--- 1. What are the standard ingredients for each pizza?
-SELECT * FROM pizza_ingredients
+-- # 1. What are the standard ingredients for each pizza?
+-- SELECT * FROM pizza_ingredients
 
--- 2. What was the most commonly added extra?
-WITH extra_count_cte AS
-  (SELECT trim(extras) AS extra_topping,
-          count(*) AS purchase_counts
-   FROM row_split_customer_orders_temp
-   WHERE extras IS NOT NULL
-   GROUP BY extras)
+-- # 2. What was the most commonly added extra?
+-- WITH extra_count_cte AS
+--   (SELECT trim(extras) AS extra_topping,
+--           count(*) AS purchase_counts
+--    FROM row_split_customer_orders_temp
+--    WHERE extras IS NOT NULL
+--    GROUP BY extras)
 
-SELECT topping_name,
-       purchase_counts
-FROM extra_count_cte
-INNER JOIN pizza_toppings ON extra_count_cte.extra_topping = pizza_toppings.topping_id;
-LIMIT 1;
+-- SELECT topping_name,
+--        purchase_counts
+-- FROM extra_count_cte
+-- INNER JOIN pizza_toppings ON extra_count_cte.extra_topping = pizza_toppings.topping_id;
+-- LIMIT 1;
 
--- 3. What was the most common exclusion?
-SELECT
-topping_name AS topping,
-COUNT(*) AS exclusion_count
-FROM customer_orders_temp ot
-JOIN pizza_toppings t ON t.topping_id = ot.exclusions
-WHERE exclusions != ''
-GROUP BY exclusions
-LIMIT 1;
+-- # 3. What was the most common exclusion?
+-- SELECT
+-- topping_name AS topping,
+-- COUNT(*) AS exclusion_count
+-- FROM customer_orders_temp ot
+-- JOIN pizza_toppings t ON t.topping_id = ot.exclusions
+-- WHERE exclusions != ''
+-- GROUP BY exclusions
+-- LIMIT 1;
 
--- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+# 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
 -- Meat Lovers
 -- Meat Lovers - Exclude Beef
 -- Meat Lovers - Extra Bacon
@@ -48,10 +48,10 @@ WHEN pizza_id = 2 AND exclusions = 4 AND extras IS NULL THEN 'Vegetarian - Exclu
 WHEN pizza_id = 2 AND exclusions IS NULL AND extras = 1 THEN 'Vegetarian - Extra Bacon'
 ELSE NULL END AS order_item
 FROM customer_orders_clean
-ORDER BY pizza_id
+ORDER BY pizza_id;
 
--- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
--- For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+# 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+# For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
 WITH ingredients_cte AS(
 SELECT 
 n.pizza_id,
@@ -84,4 +84,9 @@ END AS ingredient_list
 FROM customer_orders_clean o
 JOIN ingredients_cte ic ON ic.pizza_id = o.pizza_id
 
--- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+-- # 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+-- WITH delivered_pizza_cte AS (SELECT *
+-- FROM customer_orders_temp_2 c
+-- JOIN runner_orders_temp r
+-- ON c.order_id = r.order_id
+-- WHERE r.distance != 0)
